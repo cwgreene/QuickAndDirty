fb62869b1ee470becbda5d29e0c5e76021e6f405
Chris Greene <chris.greene@justin.tv>
2011-10-26 09:28:10 -0700

    Interval takes a start and end tag

---
+++ interval.py
@@ -0,0 +1,28 @@
+import sys
+import re
+import time
+
+def get_time(line):
+    match = re.findall("([A-Za-z]{3} [0-9]{2} ([0-9]{2}:?){3})",line)
+    timestamp = match[0][0]
+    timestruct = time.strptime(timestamp+" 2011","%b %d %H:%M:%S %Y")
+    timefloat = time.mktime(timestruct)
+    return timefloat
+    
+
+def interval(start_tag,end_tag,afile):
+    start = False
+    start_time,end_time=0,0
+    for line in afile:
+        if start == False:
+            if re.findall(start_tag,line):
+                start = True
+                start_time = get_time(line)
+        else:
+            if re.findall(end_tag,line):
+                start = False
+                end_time = get_time(line)
+                print end_time-start_time
+
+if len(sys.argv)==3:
+    interval(sys.argv[1],sys.argv[2],sys.stdin)
